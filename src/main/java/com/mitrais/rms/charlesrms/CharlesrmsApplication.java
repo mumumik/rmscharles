2e0424a1168b56f9412fec0a07e56f3b655540c5
Charles Arnesus <arnesuscharles@gmail.com>
2019-06-18 13:08:27 +0700

    Update main class
    Update main class so tomcat can run war file

--- src/main/java/com/mitrais/rms/charlesrms/CharlesrmsApplication.java
+++ src/main/java/com/mitrais/rms/charlesrms/CharlesrmsApplication.java
@@ -2,12 +2,26 @@ package com.mitrais.rms.charlesrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CharlesrmsApplication {
+public class CharlesrmsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CharlesrmsApplication.class, args);
-	}
+
+    /**
+     * Used when run as JAR
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CharlesrmsApplication.class, args);
+    }
+
+    /**
+     * Used when run as WAR
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CharlesrmsApplication.class);
+    }
 
 }
